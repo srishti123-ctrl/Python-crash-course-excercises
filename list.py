aad3c103efbf0b5dd1a31558f961c03ef15ddf7e
Srishti Poudel <poudelsrishti123@gmail.com>
2026-07-04 15:54:30 +0545

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+guest=['Dipa','Rita','Sita','Apshara']
+print(guest[0])
+print(guest[1])
+print(guest[2])
+print(guest[3])
+message=f"\nYour cupboard key is with me {guest[0].title()}."
+print(message)
+message=f"\nYou are invited in my party tonight {guest[1].title()}."
+print(message)
+message=f"\nOh God! i have frgot my purse in my home.Can you bring it to me in tonight party {guest[2].title()}."
+print(message)
+message=f"\nYou are also invited in the party tonight {guest[3].title()}."
+print(message)
