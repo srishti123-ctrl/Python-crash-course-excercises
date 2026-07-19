aa10220308556573555137e6bfecc9741e9e2cbe
Srishti Poudel <poudelsrishti123@gmail.com>
2026-07-19 11:51:42 +0545

    Add files via upload

---
+++ sortinglist.py
@@ -0,0 +1,16 @@
+print("I would like to visit : \t")
+places=['America', 'London', 'Italy', 'New Zealand', 'Japan']#list
+print(places)
+a=sorted(places)#sorting list in alphabetical order in temporary
+print(a)
+x=sorted(places ,reverse=True)#reverse sorting of sorted list
+print(x)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+print(len(places))
