276ef1dd3b460e8f39babafb458acf552f3a2645
Srishti Poudel <poudelsrishti123@gmail.com>
2026-07-19 12:11:37 +0545

    Add files via upload

---
+++ workingwithlist.py
@@ -0,0 +1,10 @@
+pizza=['Chesse pizza','Chicken pizza','pineapple pizza']
+for pizza in pizza:
+    print(f"This is {pizza.title()}")
+    print(f"I like {pizza.title()}\n")
+print("I like pizza very much.They are very tasty.")
+animal=['Dog','Cat','Horse']
+for animal in animal:
+    print(f"This is {animal.title()}")
+    print(f"{animal.title()} would make great pet\n")
+print("All this three animal would make a such a good pet")    
